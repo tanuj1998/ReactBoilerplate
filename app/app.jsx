4794799716cb8676d85364beefb3fae0b5c62e7b
tanuj1998 <tyadav@purdue.edu>
2019-09-08 13:09:21 -0400

    Update app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -2,8 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-
-
 //Load foundation
 $(document).foundation();
 //CSS
